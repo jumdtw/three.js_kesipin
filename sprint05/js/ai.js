7bad5847ac7aa1f05c912db32390608d7f734543
jumdtw <ttnmrm1m3k5g1m3r3k@gmail.com>
2018-11-18 15:29:32 +0900

    create html

---
+++ sprint05/js/ai.js
@@ -0,0 +1,69 @@
+'use strict';
+
+// 使用するAPI
+// A3RT の Talk API
+// https://a3rt.recruit-tech.co.jp/product/
+
+const API_KEY = 'DZZtUcCw3uNYAlUUrdaF8Ylbod1BDTOc';
+const API_URL = 'https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk';
+
+let output = document.getElementById('output');
+
+init();
+
+function init() {
+	document.getElementById('submitButton').addEventListener('click', submitClickHandler, false);
+}
+
+function submitClickHandler(ev) {
+	let messageText = document.getElementById('messageText');
+	let postData = `apikey=${API_KEY}&query=${messageText.value}`;
+	callAPI(API_URL, postData).then(onSuccess).catch(onError);
+	addMessage(messageText.value, false);
+	ev.preventDefault();
+	messageText.value = '';
+}
+
+function addMessage(message, isAI) {
+	let p = document.createElement('p');
+	if (isAI) {
+		p.className = 'ai';
+	}
+	p.textContent = message;
+	output.appendChild(p);
+}
+
+function onSuccess(data) {
+	let json = JSON.parse(data);
+	console.log(json);
+	addMessage(json.results[0].reply, true);
+}
+
+function onError(error) {
+	console.log(error);
+}
+
+// POSTメソッドで呼び出し、JSONで結果が返ってくる
+function callAPI(url, data) {
+	return new Promise((resolve, reject) => {
+		const req = new XMLHttpRequest();
+
+		req.open('POST', url);
+		req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		req.onload = () => {
+			if (req.readyState === 4) {
+				if (req.status === 200) {
+					resolve(req.response);
+				} else {
+					reject(req.statusText);
+				}
+			}
+		};
+ 
+		req.onerror = () => {
+			reject(req.statusText);
+		};
+
+		req.send(data);
+	});
+}
